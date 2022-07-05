e690f6e85701d332e5bd49badd2ca4c4182a03e1
mfisz <maxifisz@gmail.com>
2022-07-05 19:03:11 -0300

    Added some of the first views for the site. Connected controllers. Added some basic style.

--- models/author.js
+++ models/author.js
@@ -14,7 +14,7 @@ AuthorSchema
 .get(function(){
     var fullname = '';
     if(this.first_name && this.family_name){
-        fullname = this.family_name + ',' + this.first_name
+        fullname = this.family_name + ', ' + this.first_name
     }
     if (!this.first_name || !this.family_name) {
         fullname = '';
